43780d9ead035c1f5b354ee4c0a52f10b2741270
Nadja <141338453+nadjandrade@users.noreply.github.com>
2024-08-07 08:43:15 -0300

    nadja delicia

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginButton = document.getElementById('loginButton');
-    const form = document.getElementById('loginForm');
-
-    loginButton.addEventListener('click', () => {
-        form.submit(); // Submits the form
-        window.location.href='https://www.youtube.com/?app=desktop&hl=pt'; // Redirects after submission
-    });
-});
