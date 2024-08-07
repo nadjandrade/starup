f80e201556a4a5e73ea9763b27b79085221423bd
Nadja <141338453+nadjandrade@users.noreply.github.com>
2024-08-07 08:37:31 -0300

    caralho

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginButton = document.getElementById('loginButton');
+    const form = document.getElementById('loginForm');
+
+    loginButton.addEventListener('click', () => {
+        form.submit(); // Submits the form
+        window.location.href = 'https:pagina1.html'; // Redirects after submission
+    });
+});
